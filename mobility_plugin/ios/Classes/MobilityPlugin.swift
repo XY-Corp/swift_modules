2c33966bb338b465c528a05a3899299bd6b6d21c
lamaranelson <lamara@xy.ai>
2024-11-27 11:45:21 +0000

    added detailed logs

--- mobility_plugin/ios/Classes/MobilityPlugin.swift
+++ mobility_plugin/ios/Classes/MobilityPlugin.swift
@@ -2,16 +2,27 @@ import Flutter
 import UIKit
 import HealthKit
 
-public class SwiftMobilityPlugin: NSObject, FlutterPlugin {
-  private let healthStore = HKHealthStore()
+// Flag to enable or disable logging
+private let isLoggingEnabled = true
 
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "mobility_plugin", binaryMessenger: registrar.messenger())
-    let instance = SwiftMobilityPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
+// Logging function with a prefix
+private func log(_ message: String) {
+    if isLoggingEnabled {
+        print("[MobilityPlugin] \(message)")
+    }
+}
+
+public class SwiftMobilityPlugin: NSObject, FlutterPlugin {
+    private let healthStore = HKHealthStore()
+
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "mobility_plugin", binaryMessenger: registrar.messenger())
+        let instance = SwiftMobilityPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
+    }
 
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        log("Handling method call: \(call.method)")
         switch call.method {
         case "getAllMobilityData":
             handleGetAllMobilityData(result: result)
@@ -29,110 +40,115 @@ public class SwiftMobilityPlugin: NSObject, FlutterPlugin {
                     result(FlutterError(code: "AUTH_ERROR", message: "Authorization failed", details: nil))
                 }
             }
-                case "getMobilityDataByType":
-      getMobilityDataByType(call: call, result: result)
+        case "getMobilityDataByType":
+            getMobilityDataByType(call: call, result: result)
         default:
             result(FlutterMethodNotImplemented)
         }
     }
 
-private func getMobilityDataByType(call: FlutterMethodCall, result: @escaping FlutterResult) {
-    guard let args = call.arguments as? [String: Any],
-          let typeString = args["type"] as? String,
-          let startDateMillis = args["startDate"] as? Int,
-          let endDateMillis = args["endDate"] as? Int else {
-      result(FlutterError(code: "INVALID_ARGUMENTS", message: "Invalid arguments for getMobilityDataByType", details: nil))
-      return
+    private func getMobilityDataByType(call: FlutterMethodCall, result: @escaping FlutterResult) {
+        log("getMobilityDataByType called with arguments: \(String(describing: call.arguments))")
+        guard let args = call.arguments as? [String: Any],
+              let typeString = args["type"] as? String,
+              let startDateMillis = args["startDate"] as? Int,
+              let endDateMillis = args["endDate"] as? Int else {
+            result(FlutterError(code: "INVALID_ARGUMENTS", message: "Invalid arguments for getMobilityDataByType", details: nil))
+            return
+        }
+
+        guard let quantityType = getQuantityType(from: typeString) else {
+            result(FlutterError(code: "INVALID_TYPE", message: "Invalid type: \(typeString)", details: nil))
+            return
+        }
+
+        let startDate = Date(timeIntervalSince1970: TimeInterval(startDateMillis) / 1000)
+        let endDate = Date(timeIntervalSince1970: TimeInterval(endDateMillis) / 1000)
+
+        let predicate = HKQuery.predicateForSamples(withStart: startDate, end: endDate, options: [])
+
+        let sortDescriptor = NSSortDescriptor(key: HKSampleSortIdentifierStartDate, ascending: false)
+
+        let query = HKSampleQuery(sampleType: quantityType, predicate: predicate, limit: HKObjectQueryNoLimit, sortDescriptors: [sortDescriptor]) { (query, samples, error) in
+            if let error = error {
+                log("Error fetching data: \(error.localizedDescription)")
+                result(FlutterError(code: "ERROR_FETCHING_DATA", message: error.localizedDescription, details: nil))
+                return
+            }
+
+            guard let samples = samples as? [HKQuantitySample] else {
+                log("No samples found")
+                result([:])
+                return
+            }
+
+            let data = samples.map { sample -> [String: Any] in
+                return [
+                    "value": sample.quantity.doubleValue(for: self.unit(for: quantityType)),
+                    "startDate": Int(sample.startDate.timeIntervalSince1970 * 1000),
+                    "endDate": Int(sample.endDate.timeIntervalSince1970 * 1000),
+                ]
+            }
+
+            log("Fetched data: \(data)")
+            result(["data": data])
+        }
+
+        healthStore.execute(query)
     }
 
-    guard let quantityType = getQuantityType(from: typeString) else {
-      result(FlutterError(code: "INVALID_TYPE", message: "Invalid type: \(typeString)", details: nil))
-      return
+    private func getQuantityType(from typeString: String) -> HKQuantityType? {
+        log("Getting quantity type for: \(typeString)")
+        switch typeString {
+        case "ASYMMETRY_PERCENTAGE":
+            if #available(iOS 15.0, *) {
+                return HKQuantityType.quantityType(forIdentifier: .walkingAsymmetryPercentage)
+            }
+        case "STEP_LENGTH":
+            if #available(iOS 15.0, *) {
+                return HKQuantityType.quantityType(forIdentifier: .stepLength)
+            }
+        case "WALKING_SPEED":
+            if #available(iOS 15.0, *) {
+                return HKQuantityType.quantityType(forIdentifier: .walkingSpeed)
+            }
+        case "DOUBLE_SUPPORT_PERCENTAGE":
+            if #available(iOS 15.0, *) {
+                return HKQuantityType.quantityType(forIdentifier: .walkingDoubleSupportPercentage)
+            }
+        case "WALKING_STEADINESS":
+            if #available(iOS 15.0, *) {
+                return HKQuantityType.quantityType(forIdentifier: .walkingSteadiness)
+            }
+        default:
+            return nil
+        }
+        return nil
     }
 
-    let startDate = Date(timeIntervalSince1970: TimeInterval(startDateMillis) / 1000)
-    let endDate = Date(timeIntervalSince1970: TimeInterval(endDateMillis) / 1000)
+    private func unit(for quantityType: HKQuantityType) -> HKUnit {
+        log("Getting unit for quantity type: \(quantityType.identifier)")
+        switch quantityType.identifier {
+        case HKQuantityTypeIdentifier.walkingAsymmetryPercentage.rawValue,
+             HKQuantityTypeIdentifier.walkingDoubleSupportPercentage.rawValue:
+            return HKUnit.percent()
 
-    let predicate = HKQuery.predicateForSamples(withStart: startDate, end: endDate, options: [])
+        case HKQuantityTypeIdentifier.stepLength.rawValue:
+            return HKUnit.meter()
 
-    let sortDescriptor = NSSortDescriptor(key: HKSampleSortIdentifierStartDate, ascending: false)
+        case HKQuantityTypeIdentifier.walkingSpeed.rawValue:
+            return HKUnit(from: "m/s")
 
-    let query = HKSampleQuery(sampleType: quantityType, predicate: predicate, limit: HKObjectQueryNoLimit, sortDescriptors: [sortDescriptor]) { (query, samples, error) in
-      if let error = error {
-        result(FlutterError(code: "ERROR_FETCHING_DATA", message: error.localizedDescription, details: nil))
-        return
-      }
+        case HKQuantityTypeIdentifier.walkingSteadiness.rawValue:
+            return HKUnit.count()
 
-      guard let samples = samples as? [HKQuantitySample] else {
-        result([:])
-        return
-      }
-
-      let data = samples.map { sample -> [String: Any] in
-        return [
-          "value": sample.quantity.doubleValue(for: self.unit(for: quantityType)),
-          "startDate": Int(sample.startDate.timeIntervalSince1970 * 1000),
-          "endDate": Int(sample.endDate.timeIntervalSince1970 * 1000),
-        ]
-      }
-
-      result(["data": data])
+        default:
+            return HKUnit.count()
+        }
     }
-
-    healthStore.execute(query)
-  }
-
-  private func getQuantityType(from typeString: String) -> HKQuantityType? {
-    switch typeString {
-    case "ASYMMETRY_PERCENTAGE":
-      if #available(iOS 15.0, *) {
-        return HKQuantityType.quantityType(forIdentifier: .walkingAsymmetryPercentage)
-      }
-    case "STEP_LENGTH":
-      if #available(iOS 15.0, *) {
-        return HKQuantityType.quantityType(forIdentifier: .stepLength)
-      }
-    case "WALKING_SPEED":
-      if #available(iOS 15.0, *) {
-        return HKQuantityType.quantityType(forIdentifier: .walkingSpeed)
-      }
-    case "DOUBLE_SUPPORT_PERCENTAGE":
-      if #available(iOS 15.0, *) {
-        return HKQuantityType.quantityType(forIdentifier: .walkingDoubleSupportPercentage)
-      }
-    case "WALKING_STEADINESS":
-      if #available(iOS 15.0, *) {
-        return HKQuantityType.quantityType(forIdentifier: .walkingSteadiness)
-      }
-    default:
-      return nil
-    }
-    // Return nil if type is not available
-    return nil
-  }
-
-  private func unit(for quantityType: HKQuantityType) -> HKUnit {
-    switch quantityType.identifier {
-    case HKQuantityTypeIdentifier.walkingAsymmetryPercentage.rawValue,
-         HKQuantityTypeIdentifier.walkingDoubleSupportPercentage.rawValue:
-      return HKUnit.percent()
-
-    case HKQuantityTypeIdentifier.stepLength.rawValue:
-      return HKUnit.meter()
-
-    case HKQuantityTypeIdentifier.walkingSpeed.rawValue:
-      return HKUnit(from: "m/s")
-
-    case HKQuantityTypeIdentifier.walkingSteadiness.rawValue:
-      // Walking Steadiness is unitless; use count
-      return HKUnit.count()
-
-    default:
-      return HKUnit.count()
-    }
-  }
 
     private func requestAuthorization(completion: @escaping (Bool) -> Void) {
+        log("Requesting authorization")
         var mobilityTypes: Set<HKSampleType> = [
             HKObjectType.quantityType(forIdentifier: .walkingSpeed)!,
         ]
@@ -148,15 +164,18 @@ private func getMobilityDataByType(call: FlutterMethodCall, result: @escaping Fl
         }
 
         healthStore.requestAuthorization(toShare: nil, read: mobilityTypes) { success, _ in
+            log("Authorization success: \(success)")
             completion(success)
         }
     }
 
     private func handleGetAllMobilityData(result: @escaping FlutterResult) {
+        log("Handling getAllMobilityData")
         fetchMobilityData(startDate: nil, endDate: nil, limit: HKObjectQueryNoLimit, result: result)
     }
 
     private func handleGetMobilityData(call: FlutterMethodCall, result: @escaping FlutterResult) {
+        log("Handling getMobilityData with arguments: \(String(describing: call.arguments))")
         guard let args = call.arguments as? [String: Any],
               let startDateMillis = args["startDate"] as? Double,
               let endDateMillis = args["endDate"] as? Double else {
@@ -171,6 +190,7 @@ private func getMobilityDataByType(call: FlutterMethodCall, result: @escaping Fl
     }
 
     private func handleGetRecentMobilityData(call: FlutterMethodCall, result: @escaping FlutterResult) {
+        log("Handling getRecentMobilityData with arguments: \(String(describing: call.arguments))")
         guard let args = call.arguments as? [String: Any],
               let limit = args["limit"] as? Int else {
             result(FlutterError(code: "INVALID_ARGUMENTS", message: "Missing limit", details: nil))
@@ -181,6 +201,7 @@ private func getMobilityDataByType(call: FlutterMethodCall, result: @escaping Fl
     }
 
     private func fetchMobilityData(startDate: Date?, endDate: Date?, limit: Int, result: @escaping FlutterResult) {
+        log("Fetching mobility data with startDate: \(String(describing: startDate)), endDate: \(String(describing: endDate)), limit: \(limit)")
         if #available(iOS 13.0, *) {
             var dataTypes: [String: HKQuantityTypeIdentifier] = [
                 "walkingSpeed": .walkingSpeed,
@@ -199,89 +220,85 @@ private func getMobilityDataByType(call: FlutterMethodCall, result: @escaping Fl
         }
     }
 
-@available(iOS 13.0, *)
-private func queryMobilityData(dataTypes: [String: HKQuantityTypeIdentifier], startDate: Date?, endDate: Date?, limit: Int, result: @escaping FlutterResult) {
-    // Prepare an array to hold data type keys and their corresponding HKQuantityType
-    var dataTypesArray: [(key: String, type: HKQuantityType)] = []
-
-    // Map the identifiers to HKQuantityType and collect them with their keys
-    for (key, identifier) in dataTypes {
-        if let quantityType = HKQuantityType.quantityType(forIdentifier: identifier) {
-            dataTypesArray.append((key: key, type: quantityType))
-        }
-    }
-
-    // Create the predicate based on the provided start and end dates
-    let predicate: NSPredicate? = {
-        if let startDate = startDate, let endDate = endDate {
-            return HKQuery.predicateForSamples(withStart: startDate, end: endDate, options: [.strictStartDate, .strictEndDate])
-        } else if let startDate = startDate {
-            return HKQuery.predicateForSamples(withStart: startDate, end: nil, options: [.strictStartDate])
-        } else if let endDate = endDate {
-            return HKQuery.predicateForSamples(withStart: nil, end: endDate, options: [.strictEndDate])
-        } else {
-            return nil
-        }
-    }()
-
-    // Prepare the sort descriptor
-    let sortDescriptor = NSSortDescriptor(key: HKSampleSortIdentifierEndDate, ascending: false)
-
-    // Create an array of HKQueryDescriptor for each data type
-    var descriptors: [HKQueryDescriptor] = []
-    for (_, type) in dataTypesArray {
-        let descriptor = HKQueryDescriptor(sampleType: type, predicate: predicate)
-        descriptors.append(descriptor)
-    }
-
-    // Map sample type identifiers to their corresponding keys
-    var identifierToKey: [String: String] = [:]
-    for (key, type) in dataTypesArray {
-        identifierToKey[type.identifier] = key
-    }
-
-    // Create a single HKSampleQuery with the descriptors
-    let query = HKSampleQuery(queryDescriptors: descriptors, limit: limit, sortDescriptors: [sortDescriptor]) { query, samples, error in
-        if let error = error {
-            result(FlutterError(code: "FETCH_ERROR", message: error.localizedDescription, details: nil))
-            return
-        }
-
-        // Process the samples and organize them by their corresponding keys
+    @available(iOS 13.0, *)
+    private func queryMobilityData(dataTypes: [String: HKQuantityTypeIdentifier], startDate: Date?, endDate: Date?, limit: Int, result: @escaping FlutterResult) {
+        log("Querying mobility data")
+        let dispatchGroup = DispatchGroup()
         var allData = [String: [[String: Any]]]()
-        for sample in samples ?? [] {
-            if let quantitySample = sample as? HKQuantitySample {
-                let identifier = quantitySample.sampleType.identifier
-                guard let key = identifierToKey[identifier] else {
-                    continue
-                }
-                let unit = self.unit(for: quantitySample.quantityType)
-                let value = quantitySample.quantity.doubleValue(for: unit)
-                let startDate = Int(quantitySample.startDate.timeIntervalSince1970 * 1000)
-                let endDate = Int(quantitySample.endDate.timeIntervalSince1970 * 1000)
+        var queryError: Error?
 
-                var dataArray = allData[key] ?? []
-                dataArray.append([
-                    "value": value,
-                    "startDate": startDate,
-                    "endDate": endDate
-                ])
-                allData[key] = dataArray
+        let predicate: NSPredicate? = {
+            if let startDate = startDate, let endDate = endDate {
+                return HKQuery.predicateForSamples(withStart: startDate, end: endDate, options: [.strictStartDate, .strictEndDate])
+            } else if let startDate = startDate {
+                return HKQuery.predicateForSamples(withStart: startDate, end: nil, options: [.strictStartDate])
+            } else if let endDate = endDate {
+                return HKQuery.predicateForSamples(withStart: nil, end: endDate, options: [.strictEndDate])
+            } else {
+                return nil
             }
+        }()
+
+        let sortDescriptor = NSSortDescriptor(key: HKSampleSortIdentifierEndDate, ascending: false)
+
+        for (key, identifier) in dataTypes {
+            dispatchGroup.enter()
+            guard let quantityType = HKQuantityType.quantityType(forIdentifier: identifier) else {
+                dispatchGroup.leave()
+                continue
+            }
+
+            let query = HKSampleQuery(sampleType: quantityType, predicate: predicate, limit: limit, sortDescriptors: [sortDescriptor]) { _, samples, error in
+                defer { dispatchGroup.leave() }
+
+                if let error = error {
+                    log("Error querying data for \(key): \(error.localizedDescription)")
+                    queryError = error
+                    return
+                }
+
+                var data = [[String: Any]]()
+                let unit: HKUnit
+
+                if #available(iOS 15.0, *), identifier == .appleWalkingSteadiness {
+                    unit = HKUnit.count()
+                } else {
+                    switch identifier {
+                    case .walkingSpeed:
+                        unit = HKUnit.meter().unitDivided(by: HKUnit.second())
+                    case .walkingDoubleSupportPercentage, .walkingAsymmetryPercentage:
+                        unit = HKUnit.percent()
+                    case .walkingStepLength:
+                        unit = HKUnit.meter()
+                    default:
+                        return
+                    }
+                }
+
+                samples?.forEach { sample in
+                    if let quantitySample = sample as? HKQuantitySample {
+                        let value = quantitySample.quantity.doubleValue(for: unit)
+                        let startDate = quantitySample.startDate.timeIntervalSince1970 * 1000
+                        let endDate = quantitySample.endDate.timeIntervalSince1970 * 1000
+                        data.append([
+                            "value": value,
+                            "startDate": startDate,
+                            "endDate": endDate
+                        ])
+                    }
+                }
+                allData[key] = data
+                log("Data for \(key): \(data)")
+            }
+            healthStore.execute(query)
         }
-
-        // Return the organized data
-        result(allData)
-    }
-
-    // Execute the query
-    healthStore.execute(query)
-}
 
         dispatchGroup.notify(queue: .main) {
             if let error = queryError {
+                log("Error fetching mobility data: \(error.localizedDescription)")
                 result(FlutterError(code: "FETCH_ERROR", message: error.localizedDescription, details: nil))
             } else {
+                log("Fetched all mobility data: \(allData)")
                 result(allData)
             }
         }
@@ -289,6 +306,7 @@ private func queryMobilityData(dataTypes: [String: HKQuantityTypeIdentifier], st
 
     private func getPlatformVersion(result: @escaping FlutterResult) {
         let systemVersion = UIDevice.current.systemVersion
+        log("Getting platform version: iOS \(systemVersion)")
         result("iOS " + systemVersion)
     }
 }
